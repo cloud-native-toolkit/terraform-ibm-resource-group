12b64c9a98865a88dc45a099302c0d9e5cb8498f
Sean Sundberg <seansund@us.ibm.com>
2021-09-08 14:29:38 -0500

    Adds dependency on sync variable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 
+resource null_resource wait_for_sync {
+  provisioner "local-exec" {
+    command = "echo 'Sync: ${var.sync != null ? var.sync : ""}'"
+  }
+}
+
 resource ibm_resource_group resource_group {
   count = var.provision ? 1 : 0
+  depends_on = [null_resource.wait_for_sync]
 
   name  = var.resource_group_name
 }
