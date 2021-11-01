39dfccdb0fab0277a7152f8fe2ea548e46180808
Sean Sundberg <seansund@us.ibm.com>
2021-11-01 14:05:26 -0500

    Updates provider handling (#23)
    - Removes provider config from module root (should be done in level above module)
    - Removes ibmcloud_api_key variable as it was only needed by provider
    - Updates test to include required_providers block and provider config
    - Updates readme with example for provider handling
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-resource-groups.tf
+++ test/stages/stage1-resource-groups.tf
@@ -2,13 +2,13 @@ module "resource_group1" {
   source = "./module"
 
   resource_group_name = "test-resource-group1"
-  ibmcloud_api_key    = var.ibmcloud_api_key
+  provision           = var.resource_group_provision
 }
 
 module "resource_group2" {
   source = "./module"
 
   resource_group_name = "test-resource-group2"
-  ibmcloud_api_key    = var.ibmcloud_api_key
+  provision           = var.resource_group_provision
   sync                = module.resource_group1.sync
 }
