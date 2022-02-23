3f7cbeb9c69888160fd1b311d99eae074cc56979
Sean Sundberg <seansund@us.ibm.com>
2022-02-23 13:35:08 -0600

    Adds login for ibmcloud in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -2,6 +2,10 @@
 
 RESOURCE_GROUP_NAME=$(cat .rg_name)
 
+REGION=$(cat terraform.tfvars | grep -E "^region" | sed "s/region=//g" | sed 's/"//g')
+
+ibmcloud login -r "${REGION}" --apikey "${IBMCLOUD_API_KEY}"
+
 if ! ibmcloud resource group "${RESOURCE_GROUP_NAME}" -q; then
   echo "Resource group not found: ${RESOURCE_GROUP_NAME}"
   exit 1
