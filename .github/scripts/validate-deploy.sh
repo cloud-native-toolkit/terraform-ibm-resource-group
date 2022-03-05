3d2a05255384622903071b6908cb65bb5e9144db
Sean Sundberg <seansund@us.ibm.com>
2022-03-05 09:08:02 -0600

    Updates test container to quay.io/cloudnativetoolkit/terraform:v1.1 (#32)
    * Updates test container to quay.io/cloudnativetoolkit/terraform:v1.1
    * Uses clis submodule to download ibmcloud cli for validation
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+BIN_DIR=$(cat .bin_dir)
 RESOURCE_GROUP_NAME=$(cat .rg_name)
 
 REGION=$(cat terraform.tfvars | grep -E "^region" | sed "s/region=//g" | sed 's/"//g')
 
-ibmcloud login -r "${REGION}" --apikey "${IBMCLOUD_API_KEY}"
+${BIN_DIR}/ibmcloud login -r "${REGION}" --apikey "${IBMCLOUD_API_KEY}"
 
-if ! ibmcloud resource group "${RESOURCE_GROUP_NAME}" -q; then
+if ! ${BIN_DIR}/ibmcloud resource group "${RESOURCE_GROUP_NAME}" -q; then
   echo "Resource group not found: ${RESOURCE_GROUP_NAME}"
   exit 1
 fi
