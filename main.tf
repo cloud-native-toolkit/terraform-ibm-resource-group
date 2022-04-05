266ec41926009af782a3d13115e3d13ec97154c7
Andrew Trice <amtrice@us.ibm.com>
2022-04-05 10:24:57 -0400

    added destroy script, updated workflow logic
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -13,16 +13,34 @@ resource "random_uuid" "tag" {
 }
 
 resource null_resource resource_group {
-  count = var.provision ? 1 : 0
   depends_on = [null_resource.wait_for_sync, module.clis]
 
+  triggers = {
+    IBMCLOUD_API_KEY = base64encode(var.ibmcloud_api_key)
+    RESOURCE_GROUP_NAME  = var.resource_group_name
+    AUTOMATION_TAG  = "automation:${random_uuid.tag.result}"
+    BIN_DIR = module.clis.bin_dir
+  }
+
   provisioner "local-exec" {
+    when        = create
     command = "${path.module}/scripts/create-resource-group.sh"
     environment = {
-      IBMCLOUD_API_KEY = var.ibmcloud_api_key
-      RESOURCE_GROUP_NAME  = var.resource_group_name
-      AUTOMATION_TAG  = "automation:${random_uuid.tag.result}"
-      BIN_DIR = module.clis.bin_dir
+      IBMCLOUD_API_KEY = base64decode(self.triggers.IBMCLOUD_API_KEY)
+      RESOURCE_GROUP_NAME  = self.triggers.RESOURCE_GROUP_NAME
+      AUTOMATION_TAG  = self.triggers.AUTOMATION_TAG
+      BIN_DIR = self.triggers.BIN_DIR
+    }
+  }
+
+  provisioner "local-exec" {
+    when        = destroy
+    command = "${path.module}/scripts/delete-resource-group.sh"
+    environment = {
+      IBMCLOUD_API_KEY = base64decode(self.triggers.IBMCLOUD_API_KEY)
+      RESOURCE_GROUP_NAME  = self.triggers.RESOURCE_GROUP_NAME
+      AUTOMATION_TAG  = self.triggers.AUTOMATION_TAG
+      BIN_DIR = self.triggers.BIN_DIR
     }
   }
 }
