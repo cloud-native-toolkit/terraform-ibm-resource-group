5f3a4d70d4e259e2b9e38f15036f05e4adb01132
Andrew Trice <amtrice@us.ibm.com>
2022-04-12 15:37:38 -0400

    added semaphore to mitigate parallel execution issues (#50)
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/create-resource-group.sh
+++ scripts/create-resource-group.sh
@@ -5,6 +5,33 @@ set -e
 PATH=$BIN_DIR:$PATH
 JQ="$BIN_DIR/jq"
 
+
+
+# uses a semaphore to mitigate parallel execution problems
+SEMAPHORE="resource-group.semaphore"
+
+while true; do
+  echo "Checking for semaphore"
+  if [[ ! -f "${SEMAPHORE}" ]]; then
+    echo -n "Resource Group $RESOURCE_GROUP_NAME" > "${SEMAPHORE}"
+
+    if [[ $(cat ${SEMAPHORE}) == "Resource Group $RESOURCE_GROUP_NAME" ]]; then
+      echo "Got the semaphore. Creating resource group"
+      break
+    fi
+  fi
+
+  SLEEP_TIME=$((1 + $RANDOM % 10))
+  echo "  Waiting $SLEEP_TIME seconds for semaphore"
+  sleep $SLEEP_TIME
+done
+
+function finish {
+  rm "${SEMAPHORE}"
+}
+
+trap finish EXIT
+
 IAM_TOKEN=$(curl -s -X POST "https://iam.cloud.ibm.com/identity/token" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${IBMCLOUD_API_KEY}" | ${JQ} -r '.access_token')
