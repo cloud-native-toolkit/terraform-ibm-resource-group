5632ea81f1baf4c3aca8559816f5dd16bade3669
Sean Sundberg <seansund@us.ibm.com>
2022-04-26 11:11:05 -0500

    Updates cli submodule to pin version
    - Pins cli submodule version to 1.9.5 and uses `cloud-native-toolkit/clis/util` for registry id
    
    Closes #52
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ resource null_resource wait_for_sync {
 }
 
 module "clis" {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+  source = "cloud-native-toolkit/clis/util"
+  version = "1.9.5"
 }
 
 resource "random_uuid" "tag" {
