278a98a1580c759867f4dcf540ad57c4748d7a25
Sean Sundberg <seansund@us.ibm.com>
2022-09-21 15:17:23 -0500

    Updates test config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     ibm = {
       source = "ibm-cloud/ibm"
-      version = ">= 1.17.0"
     }
     clis = {
       source = "cloud-native-toolkit/clis"
-      version = ">= 0.2.0"
     }
   }
 }
